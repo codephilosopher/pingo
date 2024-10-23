c750332337d3e2dcbf16cf48d64290628c18532c
pradeek <pradeek.k@gmail.com>
2024-10-23 10:18:29 +0530

    Introduction to Tokenization

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+	"pingo/src/lexer"
+)
+
+func main() {
+	bytes, err := os.ReadFile("./00.lang")
+	if err != nil {
+		log.Println(err)
+	}
+	tokens := lexer.Tokonize(string(bytes))
+	for _, token := range tokens {
+		token.Debug()
+	}
+}
